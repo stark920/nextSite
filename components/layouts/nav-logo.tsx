c42fae73e0e2241b7033f3c91f2e0473cc567a69
Genos Huang <genoshuang@GenosdeMacBook-Air.local>
2024-12-30 17:21:07 +0800

    fix bug

--- components/layouts/nav-logo.tsx
+++ components/layouts/nav-logo.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import logoPng from '@/public/images/logo.png'
 import { SidebarMenu, SidebarMenuItem, useSidebar } from '@/components/ui/sidebar'
 import { cn } from '@/lib/utils'
 
@@ -15,7 +16,7 @@ export function NavLogo() {
         >
           <div className='flex aspect-square size-8 items-center justify-center'>
             <Image
-              src='/images/logo.png'
+              src={logoPng}
               alt='Logo'
               width={32}
               height={32}
@@ -24,9 +25,9 @@ export function NavLogo() {
           </div>
           <div className='grid flex-1 text-left text-sm leading-tight'>
             <span className='truncate text-base font-semibold'>
-              {process.env.NEXT_PUBLIC_SITE_TITLE}
+              前端健人
             </span>
-            <span className='truncate text-xs'>{process.env.NEXT_PUBLIC_SITE_DESCRIPTION}</span>
+            <span className='truncate text-xs'>Front-End Fitness</span>
           </div>
         </div>
       </SidebarMenuItem>
